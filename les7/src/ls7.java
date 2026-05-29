f71bd325cf9e723fea65ab3530e1969d11d351f4
ponomarev.dv <graiwers@gmail.com>
2026-05-29 12:16:53 +0400

    les7

---
+++ les7/src/ls7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ls7 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+// Класс сканер, даем имя "scanner". Выделяем память под ввод значения.
+
+        int number = scanner.nextInt();
+
+        System.out.println(number);
+    }
+}

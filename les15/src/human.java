d39a809ca90759c06acde570c3976e6ae8370788
ponomarev.dv <graiwers@gmail.com>
2026-06-04 16:34:33 +0400

    les15

---
+++ les15/src/human.java
@@ -0,0 +1,14 @@
+public class human {
+
+    public human(){
+        }
+
+    public human(int q){
+        System.out.println(q);
+    }
+
+    public human(String q){
+        System.out.println(q);
+    }
+
+}
---
+++ les15/src/ls15.java
@@ -0,0 +1,7 @@
+public class ls15 {
+    public static void main(String[] args) {
+        human one = new human();
+        human two = new human(2);
+        human three = new human("String");
+    }
+}
